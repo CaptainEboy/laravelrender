b7c6fc4bda8b180fc93f3e6e99a3070404e5a1db
CaptainEboy <emaapps.ac24@gmail.com>
2024-12-30 09:59:23 +0100

    i don taya

--- laravelrender24/scripts/00-laravel-deploy.sh
+++ laravelrender24/scripts/00-laravel-deploy.sh
@@ -9,10 +9,16 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Starting server..."
-php artisan serve --host=0.0.0.0 --port=8000 &
-
+php artisan serve --host=0.0.0.0 --port=8000 --no-reload &
+sleep 5
 echo "Running migrations..."
 php artisan migrate --force
+
+# echo "Starting server..."
+# php artisan serve --host=0.0.0.0 --port=8000 &
+
+# echo "Running migrations..."
+# php artisan migrate --force
 
 # echo "Running migrations..."
 # php artisan migrate --force
